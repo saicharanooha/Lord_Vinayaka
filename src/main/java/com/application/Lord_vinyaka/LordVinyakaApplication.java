1a24f332cf75d8bb2e69a12e754202bcdeeb13f6
snallago7 <nallagorlasaicharan7777@gmail.com>
2025-10-21 16:49:07 -0400

    Lord_Vinayaka_application with DB connection

---
+++ src/main/java/com/application/Lord_vinyaka/LordVinyakaApplication.java
@@ -0,0 +1,13 @@
+package com.application.Lord_vinyaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordVinyakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordVinyakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/Lord_vinyaka/LordVinyakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.Lord_vinyaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordVinyakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
